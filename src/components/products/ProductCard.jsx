96a1fcb3840be9fc35f62b456a116527f64e35d8
AmirAliAmin <aliaminamir77@gmail.com>
2025-11-28 21:55:57 +0500

    add the icon in card

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { FaRegHeart } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import { GoGitCompare } from "react-icons/go";
+import { BsArrowsFullscreen } from "react-icons/bs";
 import { TiStar } from "react-icons/ti";
 import { GoStar } from "react-icons/go";
 
@@ -16,11 +17,14 @@ function ProductCard({ id,name,brand,price,original,discount,category,image }) {
             <img src={image[1]?image[1]:image[0]} alt="" className='hidden w-60 rounded-t-lg group-hover:block  transition-all duration-700 hover:scale-110  object-cover' />
             </Link>
             <p className='absolute bg-primary text-white top-2 left-2 rounded-full w-7 flex items-center justify-center h-7 text-xs'>{discount}%</p>
-            <div className='absolute -top-20 group-hover:top-2 right-5  flex-col flex gap-2  transition-all duration-500 z-10'>
-              <div className='w-7 h-7 bg-white text-black flex items-center justify-center rounded-full hover:bg-primary hover:text-white '>
+            <div className='absolute -top-25 group-hover:top-2 right-5  flex-col flex gap-2  transition-all duration-500 z-10'>
+              <div className='w-7 h-7 bg-white text-black flex items-center justify-center rounded-full hover:bg-primary hover:text-white cursor-pointer'>
+                <BsArrowsFullscreen/>
+              </div>
+              <div className='w-7 h-7 bg-white text-black flex items-center justify-center rounded-full hover:bg-primary hover:text-white cursor-pointer'>
               <FaRegHeart />
               </div>
-              <div className='w-7 h-7 bg-white text-black flex items-center justify-center rounded-full hover:bg-primary hover:text-white'>
+              <div className='w-7 h-7 bg-white text-black flex items-center justify-center rounded-full hover:bg-primary hover:text-white cursor-pointer'>
               <GoGitCompare/>
               </div>
             </div>
